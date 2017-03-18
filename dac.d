e7c909a331fd445364538cac30f8252556990027
Don Ton <donton@utexas.edu>
2017-03-17 20:18:04 -0500

    .gitignore now working

--- dac.d
+++
@@ -1,5 +0,0 @@
-.\dac.o: dac.c
-.\dac.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-.\dac.o: ..\tm4c123gh6pm.h
-.\dac.o: Timer0A.h
-.\dac.o: sound.h
--- piano.d
+++
@@ -1,3 +0,0 @@
-.\piano.o: piano.c
-.\piano.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-.\piano.o: ..\tm4c123gh6pm.h
